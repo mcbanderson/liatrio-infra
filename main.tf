e0ed9cc071570e9cb1ea19af05905ffe8b201165
Max Anderson <maxwell.c.anderson@gmail.com>
2023-09-25 13:36:39 -0700

    style: Fix formatting

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "subnets" {
 }
 
 module "eks_cluster" {
-  source = "cloudposse/eks-cluster/aws"
+  source  = "cloudposse/eks-cluster/aws"
   version = "2.9.0"
 
   vpc_id                       = module.vpc.vpc_id
